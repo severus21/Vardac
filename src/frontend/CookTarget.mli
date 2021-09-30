f64788eeb3eddb89fb06b26395a07e7c83d4f6d8
Laurent Prosperi <laurent.prosperi658@orange.fr>
2021-09-30 11:43:05 +0200

    add CookTarget.mli

---
+++ src/frontend/CookTarget.mli
@@ -0,0 +1,4 @@
+(* This module translates [RawTarget] into [Target] namely it is in charge of transforming string to atom (to unify notations with IR).
+    - ensuring that each name (entrypoint, bootstrap) is properly bound by an IR binder + replace it by the corresponding atom 
+*)
+val cook_targets : Core.IR.program -> RawTarget.targets -> Core.Target.targets
